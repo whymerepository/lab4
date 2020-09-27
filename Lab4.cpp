ce468ed1a4ac987108c818283a67dbd9c914fbc1
whymerepository <71703045+whymerepository@users.noreply.github.com>
2020-09-28 00:03:34 +0300

    Add files via upload

---
+++ Lab4.cpp
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#define LENGTH 10
+
+int main()
+{
+	int arr[LENGTH];
+	int num = 0;
+
+	for (int i = 0; i < LENGTH; i++)
+		scanf_s("%d", &arr[i]);
+
+	for (int i = 0; i < LENGTH; i++)
+	{
+		if (arr[i+1] > arr[i])
+		{
+			num++;
+			for (int j = i; j < LENGTH; j++)
+			{
+				if (arr[j] > arr[j + 1])
+				{
+					i = j + 1;
+					break;
+				}
+			}
+		}
+	}
+
+	printf("\n%d", num);
+
+	return 0;
+}
